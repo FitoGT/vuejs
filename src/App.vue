f601dca038a83c719d5df275429a566db2e2113b
Adolfo Guevara <adolfo.guevara.11@gmail.com>
2018-12-20 16:29:54 -0300

    when i emit event

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@
       <titulo :titulo="titulo"></titulo>
       <nueva-tarea :tareas="tareas"></nueva-tarea>
       <lista-tareas :tareas="tareas"></lista-tareas>
+      <input-custom @emitEvent="parentMethod($event)"></input-custom>
+      {{child}}
     </div>
   </div>
 </template>
@@ -12,22 +14,29 @@
 import Titulo from './components/Titulo.vue';
 import NuevaTarea from './components/NuevaTarea.vue';
 import ListaTareas from './components/ListaTareas.vue';
+import InputCustom from './components/InputCustom.vue';
 import {store} from './main.js'
 export default{
   components:{
     Titulo,
     NuevaTarea,
-    ListaTareas
+    ListaTareas,
+    InputCustom
   },
   methods:{
       incrementarContador(){
         return this.numTareas++
+      },
+      parentMethod($event){
+        console.log($event)
+        this.child = $event;
       }
   },
   data(){
     return{
       tareas:[],
-      titulo:'Mi Lista de tareas'
+      titulo:'Mi Lista de tareas',
+      child:''
     }
   },
   created(){
---
+++ src/components/InputCustom.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <input type="text" v-model="child" @input="childMethod">
+    </div>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      child:'Mi Lista de tareas'
+    }
+  },
+  methods:{
+    childMethod(){
+      this.$emit('emitEvent',this.child);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
